739913ee486bd2540b288dd5cba7747081612b25
root <root@DESKTOP-NH2VI3D.localdomain>
2022-11-16 12:48:17 -0500

    commit tree 6e66bbbbe0542c695f17b309f05cdb347360a7a4

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -4,5 +4,21 @@
 # def is_spicy_batch(batch):
 
 def first_spicy_batch(n)
-  # your code here
+  first = 1
+  last = n
+  lastMild = nil
+  earliestSpicy = nil
+  while (lastMild == nil) do
+    mid = ((first+last)/2).floor
+    if is_spicy_batch(mid) == false 
+      if mid == earliestSpicy-1
+        lastMild = mid
+      end
+      start = mid+1
+     else 
+      last = mid-1
+      earliestSpicy = mid
+    end
+  end
+  return lastMild + 1
 end
